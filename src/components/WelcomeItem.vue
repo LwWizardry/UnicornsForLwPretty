c95dee4e14113a736fef997c70ee8dad8df776d8
Ecconia <public-commit@ecconia.com>
2023-03-08 20:40:48 +0100

    Add primitive mod list view / change layout of website

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
     <div class="details">
       <h3>
         <slot name="heading"></slot>
@@ -55,32 +52,6 @@ h3 {
     border-radius: 8px;
     width: 50px;
     height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
   }
 }
 </style>
---
+++ src/views/ModListView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>Here would be some sidebar with filters or tags, maybe a top bar?</p>
+    <p>The main content would be either a list or a grid of mod entries. The list has more details.</p>
+  </div>
+</template>
