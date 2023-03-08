52c30ed19319adb3180026fb9456d7ce9447764f
Ecconia <public-commit@ecconia.com>
2023-03-08 16:32:28 +0100

    Edit the basic about me and tinker a bit
    Nothing much going on yet, just trying.
    Eventually the template has to be replaced with our content.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>This is what this project is about...</h1>
+    <p>There are a bunch of LogicWorld mods right now. But the means of distribution are summed up: Meh.</p>
+    <br />
+    
+    <p>But lets take a look at current problems:</p>
+    <ul>
+        <li>To discover mods, one has to find a thread in the games Discord server and scroll through it. There are barely images or descriptions.</li>
+        <li>Once a mod is found, it is difficult to install it, it requires finding a Download button on GitHub and adding the right folders to the GameData folder.</li>
+        <li>There does not exist a mod changelog, nor is it easy to detect updates to a mod.</li>
+    </ul>
+    <br />
+    
+    <p>Anyway, this website here intends to address some of these projects. It is unsure how far this project will be driven. But in the best case, we get at least something to upload/download and showcase mods in a better option than Discord.</p>
+    <br />
+    
+    <p>Steps to take care of:</p>
+    <ul>
+      <li>Allow a user to log into the mod portal, to manually add mods and edit their metadata.</li>
+      <li>Display mods and their metadata, offer them as download.</li>
+      <li>Automatically extract versions from a GitHub repository.</li>
+    </ul>
+    <br />
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
 }
 </style>
