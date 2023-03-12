dcd33baf71a827540fecba49dde1c6d4a6863488
Akasus <akasus.nevelas@gmail.com>
2023-03-12 17:16:09 +0100

    added bootstrap and switched style framework to SASS

--- src/views/ModListView.vue
+++ src/views/ModListView.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <p>Here would be some sidebar with filters or tags, maybe a top bar?</p>
-    <p>The main content would be either a list or a grid of mod entries. The list has more details.</p>
+  <div class="container md:mx-auto px-4">
+    <p class="text-bg-secondary">I'm a secondary backed text</p>
+    <p class="text-bg-light">I'm a light mode text</p>
+    <p class="text-bg-primary">I'm a primary backed text</p>
+    <p class="text-bg-danger">I'm a danger backed text</p>
+    <p class="text-bg-info">I'm an info backed text</p>
+    <p class="text-bg-warning">I'm a warning backed text</p> <p class="text-bg-secondary">I'm a secondary backed text</p>
+    <p class="text-light">I'm a normal text</p>
+    <p class="text-primary">I'm a primary text</p>
+    <p class="text-secondary">I'm a secondary text</p>
+    <p class="text-danger">I'm an error text</p>
+    <p class="text-info">I'm an info text</p>
+    <p class="text-warning">I'm a warning text</p>
+    <code class="">Im' some jinky suspicious code!!</code>
+
+
+
+    <table class="table">
+      <thead class="thead">
+        <tr>
+          <th>lololo</th>
+        </tr>
+      </thead>
+      <tr>
+        <td>SomeMod</td>
+        <td>SomeVersion</td>
+      </tr>
+    </table>
+
+    <div class="card card-bg-dark">
+      <div clas="card-header">SomeMods name</div>
+    </div>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import path from 'node:path'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -8,7 +10,9 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+
     }
   }
 })
