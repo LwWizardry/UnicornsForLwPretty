f950586d8ea46a24f29a7e078a4cbe575c50e437
Ecconia <public-commit@ecconia.com>
2023-03-22 17:53:39 +0100

    Finish linking procedure
    Users can now (by following the instructions) link their browser with their LogicWorld User account.
    This does not yet store the linked result anywhere.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,31 +3,50 @@
 	<p>Logging in is done using your <a href="https://logicworld.net" target="_blank">Logic World</a> account.</p>
 	
 	<br />
-	<p>Logging in with your LW account is an four step process:</p>
+	<p>Logging in with your LW account is a three step process:</p>
 	<ol>
 		<li>
-			Make sure you are logged into your <a href="https://logicworld.net" target="_blank">Logic World</a> account or create one.<br />
-			If you loose your credentials (email, password) you will not be able to log in here either, so remember them or write them down!
+			Accept the privacy policy. Press 'Continue'.
+			<span class="check" v-if="state.loginState > LoginState.WaitingForPrivacy">✓</span>
 		</li>
-		<li>Open <a href="https://logicworld.net/view/pst-3c6860ea" target="_blank">Mod Portal Registration Thread</a> on Logic Worlds website.</li>
-		<li>Create a comment with a <i>challenge message</i> on that thread. Press the confirmation button on this page.</li>
-		<li>Delete the comment on that thread which you created just now. Press the confirmation button on this page.</li>
+		<li>
+			<ul>
+				<li>
+					<p>Make sure you are logged into your <a href="https://logicworld.net" target="_blank">Logic World</a> account or create one.</p>
+					<p>If you loose your credentials (email, password) you will not be able to log in here either, so remember them or write them down!</p>
+				</li>
+				<li>
+					<p>Open <a href="https://logicworld.net/view/pst-3c6860ea" target="_blank">Mod Portal Registration Thread</a> on the Logic Worlds website.</p>
+				</li>
+				<li>
+					<p>
+						Create a comment with a '<i>challenge message</i>' on that thread. Press 'Continue'.
+						<span class="check" v-if="state.loginState > LoginState.WaitingForComment">✓</span>
+					</p>
+				</li>
+			</ul>
+		</li>
+		<li>
+			Delete the comment on that thread which you created just now. Press 'Continue' to finish the log in.
+			<span class="check" v-if="state.loginState > LoginState.WaitingForDeletion">✓</span>
+		</li>
 	</ol>
-	
-	<br />
-	<p>By logging in, you agree to our Privacy Policy (that does not exist yet)</p>
 	
 	<br />
 	<p>Instruction:</p>
 	<div class="instruction-box">
-		<div v-if="state.loginState === LoginState.WaitingForChallenge">
-			Server has not yet sent a challenge...
+		<div v-if="state.loginState === LoginState.WaitingForPrivacy">
+			<p><input type="checkbox" v-model="state.acceptPP"> I accept the <a href="/privacy-policy" target="_blank">Privacy Policy</a> (required)</p>
+			<br />
+			<button :disabled="!state.acceptPP" @click="acceptPrivacyPolicy">Continue</button>
 		</div>
 		<div v-else-if="state.loginState === LoginState.WaitingForComment">
 			<p>Open Logic World Forum Thread <a href="https://logicworld.net/view/pst-3c6860ea" target="_blank">Mod Portal Registration</a> and create a comment with following content:</p>
-			<p><span class="challenge-text">{{ state.serverChallenge.challenge }}</span> <button @click="copyToClipboard">Copy!</button></p>
+			<p><span class="challenge-text">{{ state.serverChallenge?.challenge }}</span> <button @click="copyToClipboard">Copy!</button></p>
 			<br />
-			<p>Once done, confirm by pressing this button: <button @click="createdComment">I created that comment!</button></p>
+			<p>For your own safety: <b>Do not edit the comment containing your challenge</b> (reload this page if you messed up).</p>
+			<br />
+			<button @click="createdComment">Continue</button>
 		</div>
 		<div v-else-if="state.loginState === LoginState.WaitingForDeletion">
 			<div>
@@ -38,17 +57,26 @@
 					</li>
 				</ul>
 			</div>
-			<p>Press here to check if all your comments are deleted: <button @click="deletedComment">I deleted all my comments!</button></p>
+			<br />
+			<button @click="deletedComment">Continue</button>
 		</div>
+		<div v-else-if="state.loginState === LoginState.LoggedIn">
+				<p>You are logged in!</p>
+		</div>
+	</div>
+	<br />
+	
+	<div v-if="state.lastError">
+		<p>Internal API error:</p>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive } from "vue";
+import { reactive } from "vue";
 import axios, { AxiosError } from "axios";
 
 enum LoginState {
-	WaitingForChallenge,
+	WaitingForPrivacy,
 	WaitingForComment,
 	WaitingForDeletion,
 	LoggedIn,
@@ -64,15 +92,17 @@ interface MessageToDelete {
 	content: string,
 }
 
+//### STATE #####
+
 const state = reactive({
-	loginState: LoginState.WaitingForChallenge,
+	acceptPP: false,
+	loginState: LoginState.WaitingForPrivacy,
 	serverChallenge: null as ServerChallenge|null,
 	messagesToDelete: null as MessageToDelete[]|null,
+	lastError: null as string|null,
 });
 
-onMounted(() => {
-	requestServerChallenge();
-});
+//### HELPERS #####
 
 function copyToClipboard() {
 	if(!state.serverChallenge) {
@@ -82,112 +112,220 @@ function copyToClipboard() {
 	navigator.clipboard.writeText(state.serverChallenge.challenge);
 }
 
-async function requestServerChallenge() {
-	//Load the challenge session from the server:
-	const remote = import.meta.env.VITE_BACKEND + '/auth/login/new';
-	console.log("Performing API request to: ", remote); //TODO: Remove this message eventually.
-	try {
-		const response = await axios.get(remote);
-		//TODO: Validate data sent by server:
-		state.serverChallenge = response.data;
-		state.loginState = LoginState.WaitingForComment;
-	} catch (e) {
-		//TODO: Regardless, handle the error visually!
-		if(axios.isAxiosError(e)) {
-			debugAxiosError(e as AxiosError);
-		} else {
-			console.error("GenericError: ", e);
+function updateCommentsToDelete(commentsToDelete: any) {
+	if(!commentsToDelete || !(commentsToDelete instanceof Array)) {
+		return false;
+	}
+	for (const comment of commentsToDelete) {
+		if(typeof(comment.id) != "string" || typeof(comment.content) != "string") {
+			return false;
 		}
 	}
+	
+	state.messagesToDelete = commentsToDelete;
+	return true;
+}
+
+//### STATE-SWITCHER #####
+
+async function acceptPrivacyPolicy() {
+	if(!state.acceptPP) {
+		//This should not be possible. Nevertheless, stop here!
+		return;
+	}
+	
+	//Load the challenge session from the server:
+	const response = await performAPIRequest('/auth/login/new?privacy-policy=accept');
+	if(!response) {
+		return; //Failed!
+	}
+	//We got a data block!
+	if(!response.challenge || !response.session || typeof(response.challenge) != "string" || typeof(response.session) != "string") {
+		console.error("API returned nonsense:", response);
+		//TODO: notify user of issue!
+	}
+	
+	state.serverChallenge = response;
+	state.loginState = LoginState.WaitingForComment;
 }
 
 async function createdComment() {
-	if(!state.serverChallenge) {
-		//Whoops!
+	if(!state.acceptPP || state.loginState != LoginState.WaitingForComment || !state.serverChallenge) {
+		//Something went terribly wrong, it should not be possible to even trigger this.
 		return;
 	}
-	const remote = import.meta.env.VITE_BACKEND + '/auth/login/created';
-	console.log("Performing API request to: ", remote); //TODO: Remove this message eventually.
-	try {
-		const response = await axios.get(remote + '?session=' + state.serverChallenge.session);
-		//TODO: Validate data sent by server:
-		const data = response.data;
-		if(data.ok) {
-			state.messagesToDelete = response.data.messagesToDelete,
-			state.loginState = LoginState.WaitingForDeletion;
-		} else {
-			const failReason = data.reason;
-			if(failReason == "timeout") {
-				console.log("Took too long to create the comment, try new challenge!");
-			} else if(failReason == "missing") {
-				console.log("Could not find the message");
-			} else {
-				console.error("API returned unknown failure reason for checking comment creation:", failReason);
-			}
-		}
-	} catch (e) {
-		//TODO: Regardless, handle the error visually!
-		if(axios.isAxiosError(e)) {
-			debugAxiosError(e as AxiosError);
-		} else {
-			console.error("GenericError: ", e);
-		}
+	
+	const response = await performAPIRequest('/auth/login/created?session=' + encodeURIComponent(state.serverChallenge.session));
+	if(!response) {
+		return; //Failed!
 	}
-}
-
-function debugAxiosError(error: AxiosError) {
-	if(error.code == AxiosError.ERR_BAD_REQUEST) {
-		if(error.message == "Request failed with status code 404") {
-			console.error("Server claims that '", error.request.responseURL, "' does not exist and returned a 404 error.");
-		} else {
-			console.error("Server claims that a bad request has been performed. Detailed message:", error.message);
-		}
-		return;
-	} else if(error.code == AxiosError.ERR_NETWORK) {
-		console.error("A generic network access has occurred, more information in console?");
+	//We got a data block!
+	
+	if(typeof(response.author) != "string") {
+		console.error("Weird API data response:", response);
 		return;
 	}
-	console.error("Axios error{ Code: '", error.code, "' Message: '", error.message, "'}");
+	if(!updateCommentsToDelete(response.messagesToDelete)) {
+		console.error("Weird API data response:", response);
+		return;
+	}
+	state.loginState = LoginState.WaitingForDeletion;
 }
 
 async function deletedComment() {
-	if(!state.serverChallenge) {
-		//Whoops!
+	if(!state.acceptPP || state.loginState != LoginState.WaitingForDeletion || !state.serverChallenge) {
+		//Something went terribly wrong, it should not be possible to even trigger this.
 		return;
 	}
-	const remote = import.meta.env.VITE_BACKEND + '/auth/login/deleted';
-	console.log("Performing API request to: ", remote); //TODO: Remove this message eventually.
+	
+	const response = await performAPIRequest('/auth/login/deleted?session=' + encodeURIComponent(state.serverChallenge.session));
+	if(!response) {
+		return; //Failed!
+	}
+	//We got a data block!
+	
+	//TODO: Parse login data JWT etc as soon as the backend has code for that!
+	
+	state.loginState = LoginState.LoggedIn;
+}
+
+//### API-ACCESS #####
+
+async function performAPIRequest(path: string) {
+	const remote = import.meta.env.VITE_BACKEND + path;
+	//console.log("Performing API request to: ", remote);
 	try {
-		const response = await axios.post(remote, {
-			"session": state.serverChallenge.session,
-		});
-		//TODO: Validate data sent by server:
-		const data = response.data;
-		if(data.ok) {
-			state.messagesToDelete = response.data.messagesToDelete,
-				state.loginState = LoginState.WaitingForDeletion;
-		} else {
-			const failReason = data.reason;
-			if(failReason == "timeout") {
-				console.log("Took too long to create the comment, try new challenge! (Aka, reload...)");
-			} else if(failReason == "missing") {
-				console.log("Could not find the message");
-			} else {
-				console.error("API returned unknown failure reason for checking comment creation:", failReason);
-			}
+		const response = await axios.get(remote);
+		if(!response.data) {
+			//How is there no data?
+			console.error("API request yielded " + response.status + " but no content was delivered...");
+			//TODO: Notify user of error...
+			return undefined;
 		}
+		//Got a 200 response with content.
+		const data = response.data?.data;
+		if(!data) {
+			//But the expected 'data' block from the API is missing!
+			//Check for error/failure:
+			debugRequestContent(response.data);
+			return undefined;
+		}
+		//Got data block, forward it:
+		return data;
 	} catch (e) {
-		//TODO: Regardless, handle the error visually!
+		//Something went wrong!
 		if(axios.isAxiosError(e)) {
 			debugAxiosError(e as AxiosError);
 		} else {
-			console.error("GenericError: ", e);
+			console.error("Exception was thrown while performing API request.", e);
+			//TODO: Notify user of error...
 		}
+	}
+	return undefined;
+}
+
+function debugAxiosError(error: AxiosError) {
+	if(error.response?.data) {
+		//We got a response from the server. Hence, process that instead:
+		debugRequestContent(error.response.data);
+		return;
+	}
+	//At this point, the server never sent response content that we could process.
+	// Print generic errors:
+	
+	if(error.code == AxiosError.ERR_NETWORK) {
+		console.error("Network error while talking to API. Check console for details!");
+		console.error("Full error:", error);
+		return;
+	}
+	
+	if(!error.response) {
+		//How is this possible? How would axios take care of this?
+		// Just dump the whole thing...
+		console.log("Received AxiosError without response from the server:", error);
+		return;
+	}
+	
+	const response = error.response;
+	if(response.status == 404) {
+		console.error("Server claims that '" + error.request.responseURL + "' does not exist and returned a 404 error.");
+	} else if(response.status == 405) {
+		console.error("Server claims that a bad request type has been used on '" + error.request.responseURL + "'.");
+	} else {
+		console.error("Axios error{ Code: '", error.code, "' Message: '", error.message, "'}");
+		console.error("Full error:", error);
+	}
+}
+
+function debugRequestContent(content: any) {
+	if(content.error) {
+		const error = content.error;
+		if(error.type == "bad-request" && typeof(error.message) == "string") {
+			//TODO: Notify user!
+			console.error("API returned bad request with message:", error.message);
+		} else if(error.type == "internal-descriptive" && typeof(error.message) == "string") {
+			//TODO: Notify user!
+			console.error("API had internal error with message:", error.message);
+		} else if(error.type == "internal"
+			&& typeof(error.class) == "string"
+			&& typeof(error.code) != "undefined"
+			&& typeof(error.message) == "string"
+			&& typeof(error["trace"]) == "string"
+		) {
+			//TODO: Notify user!
+			console.error("Backend threw unexpected exception while performing API request:\n",
+				error.class, "(", error.code, "):", error.message, "\n\n",
+				error.trace
+			);
+		} else {
+			//TODO: Notify user!
+			console.error("API returned unknown error:", content.error);
+		}
+	} else if(content.failure) {
+		const failure = content.failure;
+		if(failure.actions)
+		{
+			for (const entry of failure.actions) {
+				const action = entry.action;
+				if(!action) {
+					continue;
+				}
+				if(action == "new-session") {
+					//Reset state back to privacy policy:
+					state.loginState = LoginState.WaitingForPrivacy;
+					state.serverChallenge = null;
+					state.messagesToDelete = null;
+					//Pretend the user acknowledged that.
+					acceptPrivacyPolicy();
+				} else if(action == "update-comments") {
+					if(!updateCommentsToDelete(entry.comments)) {
+						console.error("Server sent malformed/invalid comments to delete update: ", entry.comments);
+					}
+				} else {
+					//TODO: Notify user!
+					console.error("UNKNOWN ACTION:", action);
+				}
+			}
+		}
+		if(failure["user-error"] && typeof(failure["user-error"]) == "string") {
+			//TODO: Notify user!
+			console.error("API returned user error:", failure['user-error']);
+		} else {
+			//TODO: Notify user!
+			console.error("API returned unknown failure:", content.failure);
+		}
+	} else {
+		//Huh?
+		console.error("API returned weird content:", content);
 	}
 }
 </script>
 
 <style scoped>
+	b {
+		color: #ddd;
+	}
+
 	.instruction-box {
 		border: #aaa solid 1px;
 		border-radius: 4px;
@@ -215,5 +353,9 @@ async function deletedComment() {
 	button:active {
 		background-color: #444;
 		color: #bbb;
+	}
+	button:disabled {
+		background-color: #222;
+		border-color: #040;
 	}
 </style>
