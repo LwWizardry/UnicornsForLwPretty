2fee41ebe09a1a6ab706bbdc09625a26ad846415
Ecconia <public-commit@ecconia.com>
2023-04-29 00:30:05 +0200

    Add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// @ts-ignore
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      vitePluginFaviconsInject('./src/assets/logo.svg'),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
